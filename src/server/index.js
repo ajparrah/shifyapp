4d5b144ad624d0ece7443739deff8bae600cb031
Alejandro Parra <alejandro_parra_15@hotmail.com>
2022-01-23 21:48:41 -0400

    feat: Add cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import pinoLogger, { Logger } from '../config/logger.js';
 import routes from '../routes/index.js';
+import cors from 'cors';
 
 const app = express();
 if (process.env.NODE_ENV !== 'test') {
   app.use(pinoLogger);
 }
+
+app.use(cors());
 
 const PORT = process.env.PORT || 3300;
 
